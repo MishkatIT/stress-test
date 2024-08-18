c96c236c372407021449b75d28216700af11b088
Mishkatul Islam <mishkatcse1@gmail.com>
2024-08-18 21:59:59 +0600

    first update

---
+++ correct.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t; cin >> t;
+  for (int i = 1; i <= t; i++) {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int j = 1; j <= n; j++) {
+      int x;
+      cin >> x;
+      sum += x;
+    }
+    cout << sum << '\n';
+  }
+  return 0;
+}
---
+++ generator.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 20;
+
+// NEVER use rand() function because rand() will always generate same random values even if you run the same code multiple times
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count()); // using current time as seed, so it will always generate different random values if you run this code multiple times
+long long get_rand(long long l, long long r) { // random number from l to r
+  assert(l <= r);
+  return l + rnd() % (r - l + 1);
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  cout << t << '\n';
+  for (int i = 1; i <= t; i++) {
+    int n = rnd() % 13 + 1;
+    cout << n << '\n';
+    for (int j = 1; j <= n; j++) {
+      cout << get_rand(1, N) << ' ';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
